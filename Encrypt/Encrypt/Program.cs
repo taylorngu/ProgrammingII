6d211bbf62358e2249905479fefabceb52b931d5
taylorngu <38481437+taylorngu@users.noreply.github.com>
2018-04-20 12:06:33 -0400

    Add files via upload

---
+++ Encrypt/Encrypt/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Encrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                string phrase = "";
+                int shift = 0;
+
+                do
+                {
+                    StringBuilder sb = new StringBuilder();
+
+                    Console.WriteLine("Enter a positive whole number greater than 0 to shift by. Enter 'QQQ' to stop.");
+                    shift = Convert.ToInt16(Console.ReadLine());
+
+                    if (shift < 0)
+                    {
+                        throw new Exception();
+                    }
+
+                    Console.WriteLine("Input a phrase or series of numbers to encrypt.");
+                    phrase = Console.ReadLine();
+
+                    foreach (char c in phrase)
+                    {
+                        char newChar = ' ';
+
+                        if (char.IsLetterOrDigit(c) == true)
+                        {
+                            if (char.IsLower(c))
+                            {
+                                newChar = LowerShift(c, shift);
+                            }
+                            else if (char.IsUpper(c))
+                            {
+                                newChar = UpperShift(c, shift);
+                            }
+                            else
+                            {
+                                newChar = NumShift(c, shift);
+                            }
+
+                            sb.Append(newChar);
+                        }
+                        else
+                        {
+                            sb.Append(c);
+                        }
+
+                    }
+
+                    Console.WriteLine("The encrypted phrase is:\n{0}", sb);
+
+                } while (phrase != "QQQ");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter valid values.");
+            }
+        }
+
+        public static char LowerShift(char c, int shift)
+        {
+            int place = c + shift;
+
+            while (place > 'z')
+            {
+                place = ('a' - 1) + (place - 'z');
+            }
+
+            return (char)place;
+        }
+
+        public static char UpperShift(char c, int shift)
+        {
+            int place = c + shift;
+
+            while (place > 'Z')
+            {
+                place = ('A' - 1) + (place - 'Z');
+            }
+
+            return (char)place;
+        }
+
+        public static char NumShift(char c, int shift)
+        {
+            int place = c + shift;
+
+            while (place > '9')
+            {
+                place = ('0' - 1) + (place - '9');
+            }
+
+            return (char)place;
+        }
+
+    }
+}
+
---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Palindrome
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string phrase = "";
+            int mirCount = 0;
+            int pivCount = 0;
+            int notPalCount = 0;
+
+            Console.WriteLine("Enter either a word or a phrase. Enter QQQ to stop.");
+            phrase = Console.ReadLine();
+
+            do
+            {
+
+                StringBuilder sb = new StringBuilder();
+                foreach (char c in phrase)
+                {
+                    if (char.IsLetterOrDigit(c))
+                    {
+                        sb.Append(char.ToLower(c));
+                    }
+                }
+
+                string phraseLetters = sb.ToString();
+
+                char[] phraseConvert = phraseLetters.ToCharArray();
+                Array.Reverse(phraseConvert);
+                string phrasePal = new string(phraseConvert);
+
+                if (phraseLetters == phrasePal)
+                {
+                    if (phraseLetters.Length % 2 == 0)
+                    {
+                        Console.WriteLine("The phrase is a MIRROR palindrome.");
+                        mirCount++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("The phrase is a PIVOT palindrome.");
+                        pivCount++;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("The phrase is NOT a palindrome.");
+                    notPalCount++;
+                }
+
+                Console.WriteLine("Enter either a word or a phrase. Enter QQQ to stop.");
+                phrase = Console.ReadLine();
+
+            } while (phrase != "QQQ");
+
+            Console.WriteLine(string.Format("Number of MIRROR palindromes entered: {0}", mirCount));
+            Console.WriteLine(string.Format("Number of PIVOT palindromes entered: {0}", pivCount));
+            Console.WriteLine(string.Format("Number of NON palindromes entered: {0}", notPalCount));
+
+        }
+    }
+}
+
