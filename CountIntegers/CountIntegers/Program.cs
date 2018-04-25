2ac7269f0586be70dbee46dd104ea98cb1d2e66a
taylorngu <38481437+taylorngu@users.noreply.github.com>
2018-04-25 13:59:33 -0400

    Update Program.cs

--- CountIntegers/CountIntegers/Program.cs
+++ CountIntegers/CountIntegers/Program.cs
@@ -11,8 +11,9 @@ namespace CountIntegers
         static void Main(string[] args)
         {
             string[] rawNumbers = File.ReadAllText(Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "\\testRead.txt").Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
-
             int[] numbers = Array.ConvertAll(rawNumbers, int.Parse);
+            int key = 1;
+            int occur = match.Count();
             
             var match = from i in numbers
                         orderby i 
@@ -20,12 +21,7 @@ namespace CountIntegers
                         let count = x.Count()
                         select count;
 
-            int occur = match.Count();
-
             Console.WriteLine();
-
-            int key = 1;
-
             foreach (int i in match)
             {
                 Console.WriteLine(String.Format("Key {0} occured {1} times.", key++, i));
